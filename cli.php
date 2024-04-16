c01d4fcb9d043059ca001c9f2526399a3d38e986
Egidijus <egidijus04petrauskas@gmailc.com>
2024-04-16 20:26:58 +0300

    before registration and authentication

---
+++ cli.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$driver = 'mysql';
+$config = [
+  'host' => '127.0.0.1',
+  'port' => 3306,
+  'dbname' => 'phpiggy',
+];
+$username = 'root';
+$password = '';
+
+$db = new Database($driver, $config, $username, $password);
+
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
